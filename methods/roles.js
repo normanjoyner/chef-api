70212048aa16a218a1992df84ee6429d412cd836
Norman Joyner <norman.joyner@gmail.com>
2013-12-10 10:24:55 -0500

    Add documentation links and not implemented route links

--- methods/roles.js
+++ methods/roles.js
@@ -3,17 +3,28 @@ exports.methods = function(config){
     var http_methods = require([__dirname, "..", "http_methods"].join("/")).http_methods(config);
 
     return {
+        // http://docs.opscode.com/api_chef_server_roles.html#get
         getRoles: function(fn){
             http_methods.get([config.host_url, "roles"].join("/"), null, function(err, response){
                 fn(err, response);
             });
         },
 
+        // http://docs.opscode.com/api_chef_server_roles_name.html#get
         getRole: function(role, fn){
             http_methods.get([config.host_url, "roles", role].join("/"), null, function(err, response){
                 fn(err, response);
             });
         }
+
+        /*
+            not yet implemented:
+            // http://docs.opscode.com/api_chef_server_roles.html#post
+            // http://docs.opscode.com/api_chef_server_roles_name.html#put
+            // http://docs.opscode.com/api_chef_server_roles_name.html#delete
+            // http://docs.opscode.com/api_chef_server_roles_name_environments.html#get
+            // http://docs.opscode.com/api_chef_server_roles_name_environments_name.html#get
+        */
     }
 
 }
