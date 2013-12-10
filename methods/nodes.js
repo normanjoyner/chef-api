c635fad95024ffed70e4c150d2486e402a12b577
Norman Joyner <norman.joyner@gmail.com>
2013-12-10 10:25:31 -0500

    Add documentation links and fix createNode http call

--- methods/nodes.js
+++ methods/nodes.js
@@ -3,12 +3,14 @@ exports.methods = function(config){
     var http_methods = require([__dirname, "..", "http_methods"].join("/")).http_methods(config);
 
     return {
+        // http://docs.opscode.com/api_chef_server_nodes.html#get
         getNodes: function(fn){
             http_methods.get([config.host_url, "nodes"].join("/"), null, function(err, response){
                 fn(err, response);
             });
         },
 
+        // http://docs.opscode.com/api_chef_server_nodes_name.html#get
         getNode: function(node, fn){
             http_methods.get([config.host_url, "nodes", node].join("/"), null, function(err, response){
                 fn(err, response);
@@ -21,18 +23,21 @@ exports.methods = function(config){
             });
         },
 
-        createNode: function(body, fn){
-            http_methods.post([config.host_url, "nodes"].join("/"), body, function(err, response){
+        // http://docs.opscode.com/api_chef_server_nodes.html#post
+        createNode: function(data, fn){
+            http_methods.post([config.host_url, "nodes"].join("/"), data, function(err, response){
                 fn(err, response);
             });
         },
 
+        // http://docs.opscode.com/api_chef_server_nodes_name.html#delete
         deleteNode: function(node, fn){
             http_methods.del([config.host_url, "nodes", node].join("/"), function(err, response){
                 fn(err, response);
             });
         },
 
+        // http://docs.opscode.com/api_chef_server_nodes_name.html#put
         editNode: function(node, data, fn){
             http_methods.put([config.host_url, "nodes", node].join("/"), data, function(err, response){
                 fn(err, response);
