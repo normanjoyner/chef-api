de8ea70181d3162b7b7e0939e3f869eeb2de8fb4
Norman Joyner <norman.joyner@gmail.com>
2013-12-10 10:23:52 -0500

    Add documentation link

--- methods/cookbooks.js
+++ methods/cookbooks.js
@@ -3,30 +3,35 @@ exports.methods = function(config){
     var http_methods = require([__dirname, "..", "http_methods"].join("/")).http_methods(config);
 
     return {
-        getCookbooks: function(fn){
+        // http://docs.opscode.com/api_chef_server_cookbooks.html#get
+        getCookbooks: function(qs, fn){
             http_methods.get([config.host_url, "cookbooks"].join("/"), null, function(err, response){
                 fn(err, response);
             });
         },
 
-        getCookbook: function(cookbook, fn){
+        // http://docs.opscode.com/api_chef_server_cookbooks_name.html#get
+        getCookbook: function(cookbook, qs, fn){
             http_methods.get([config.host_url, "cookbooks", cookbook].join("/"), null, function(err, response){
                 fn(err, response);
             });
         },
 
+        // http://docs.opscode.com/api_chef_server_cookbooks_version.html#get
         getCookbookVersion: function(cookbook, version, fn){
             http_methods.get([config.host_url, "cookbooks", cookbook, version].join("/"), null, function(err, response){
                 fn(err, response);
             });
         },
 
+        // http://docs.opscode.com/api_chef_server_cookbooks_version.html#put
         editCookbookVersion: function(cookbook, version, data, fn){
             http_methods.put([config.host_url, "cookbooks", cookbook, version].join("/"), data, function(err, response){
                 fn(err, response);
             });
         },
 
+        // http://docs.opscode.com/api_chef_server_cookbooks_version.html#delete
         deleteCookbookVersion: function(cookbook, version, data, fn){
             http_methods.del([config.host_url, "cookbooks", cookbook, version].join("/"), data, function(err, response){
                 fn(err, response);
