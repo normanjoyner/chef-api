68a0e794b74d6eaa81f5591e3b328e40cb9e6f4e
Norman Joyner <norman.joyner@gmail.com>
2013-12-10 10:24:30 -0500

    Add documentation links

--- methods/search.js
+++ methods/search.js
@@ -3,16 +3,22 @@ exports.methods = function(config){
     var http_methods = require([__dirname, "..", "http_methods"].join("/")).http_methods(config);
 
     return {
+        // http://docs.opscode.com/api_chef_server_search.html#get
         getSearch: function(fn){
             http_methods.get([config.host_url, "search"].join("/"), null, function(err, response){
                 fn(err, response);
             });
         }, 
 
+        // http://docs.opscode.com/api_chef_server_search_index.html#get
         getSearchIndex: function(index, qs, fn){
             http_methods.get([config.host_url, "search", index].join("/"), qs, function(err, response){
                 fn(err, response);
             });
         }
+        /*
+            not yet implemented:
+            // http://docs.opscode.com/api_chef_server_search_index.html#post
+        */
     }
 }
