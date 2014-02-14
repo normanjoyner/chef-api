a5bbcd8df07dd673573df93f1ba6eea45aadc8d7
Mark Allen <mark@markcallen.com>
2014-02-14 07:44:00 -0800

    Removed as this was fixed will pull #4

--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 var fs = require("fs");
 var ChefApi = require("./chef-api");
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
-
 exports = module.exports = function(){
     var object = ChefApi.getObject();
     object.version = "0.3.0";
