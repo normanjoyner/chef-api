a2327139e97f3222f6b21757243a3fb473241ce6
Raman Shalupau <raman.shalupau@gmail.com>
2014-03-10 13:55:20 +0800

    added postSearchIndex

--- methods/search.js
+++ methods/search.js
@@ -8,17 +8,19 @@ exports.methods = function(config){
             http_methods.get([config.host_url, "search"].join("/"), null, function(err, response){
                 return fn(err, response);
             });
-        }, 
+        },
 
         // http://docs.opscode.com/api_chef_server_search_index.html#get
         getSearchIndex: function(index, qs, fn){
             http_methods.get([config.host_url, "search", index].join("/"), qs, function(err, response){
                 return fn(err, response);
             });
+        },
+        // http://docs.opscode.com/api_chef_server_search_index.html#post
+        postSearchIndex: function(index, data, fn){
+            http_methods.post([config.host_url, "search", index].join("/"), data, function(err, response){
+                return fn(err, response);
+            });
         }
-        /*
-            not yet implemented:
-            // http://docs.opscode.com/api_chef_server_search_index.html#post
-        */
     }
 }
