79728eca704d2a21e8e6d78d52bb1446a5020649
Norman Joyner <norman.joyner@gmail.com>
2014-03-11 11:23:48 -0400

    Rename methods and implement partialSearch

--- methods/search.js
+++ methods/search.js
@@ -1,24 +1,29 @@
+var _ = require("lodash");
+
 exports.methods = function(config){
 
     var http_methods = require([__dirname, "..", "http_methods"].join("/")).http_methods(config);
 
     return {
         // http://docs.opscode.com/api_chef_server_search.html#get
-        getSearch: function(fn){
+        getSearchIndices: function(fn){
             http_methods.get([config.host_url, "search"].join("/"), null, function(err, response){
                 return fn(err, response);
             });
         }, 
 
         // http://docs.opscode.com/api_chef_server_search_index.html#get
-        getSearchIndex: function(index, qs, fn){
+        search: function(index, qs, fn){
             http_methods.get([config.host_url, "search", index].join("/"), qs, function(err, response){
                 return fn(err, response);
             });
+        },
+
+        // http://docs.opscode.com/api_chef_server_search_index.html#post
+        partialSearch: function(index, qs, data, fn){
+            http_methods.post([config.host_url, "search", index].join("/"), qs, data, function(err, response){
+                return fn(err, response);
+            });
         }
-        /*
-            not yet implemented:
-            // http://docs.opscode.com/api_chef_server_search_index.html#post
-        */
     }
 }
